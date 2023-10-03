d5d2725e8f212783ed258711e8cc9d3b7f7591c9
Bamlaku <bamlakhiruya@gmail.com>
2023-10-03 22:33:25 +0300

    commit

---
+++ datastructures/BST/bst.py
@@ -0,0 +1,15 @@
+#different bst implementations 
+
+class Node: 
+    def __init__(self, key, left = None, right = None):
+        self.key = key
+        self.left = left
+        self.right = right
+
+    def insert(self, key):
+        root = Node(key)
+
+        if root 
+
+
+        
