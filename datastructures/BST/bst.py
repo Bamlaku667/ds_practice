c5e0a9c1d35f692f02d401bdeba5d77eb5c60f58
Bamlaku <bamlakhiruya@gmail.com>
2023-10-06 15:11:38 +0300

    inorder traversal both recursive and iterative

--- datastructures/BST/bst.py
+++ datastructures/BST/bst.py
@@ -7,66 +7,99 @@ class Node:
         self.right = right
 
 
+class BST: 
+    root = None
 
+    #using iterative method 
+    def insert(self, key):
+        newNode = Node(key)
+        if self.root is None:
+            self.root = newNode
+            return 
+        prev_node = None
+        temp_node = self.root
+        while temp_node != None:
+            if newNode.key > temp_node.key:
+                prev = temp_node
+                temp_node = temp_node.right
+            elif newNode.key < temp_node.key:
+                prev = temp_node
+                temp_node = temp_node.left
 
-def insert(node, key):
-    if node is None:
-        return Node(key)
-    if key < node.key:
-        node.left = insert(node.left, key)
-    elif key > node.key:
-        node.right = insert(node.right, key)
+        if newNode.key < prev.key:
+            prev.left = newNode
+        else: 
+            prev.right = newNode
 
-    return node
-
-
-
-def inorder(node):
-    if node is not None:
-        inorder(node.left)
-        print(node.key , end = " ")
-        inorder(node.right)
-
-def preorder(node):
-    if node is not None: 
-        print(node.key, end = " ")
-        preorder(node.left)
-        preorder(node.right)
-
-def postorder(node):
-    if node is not None: 
-        postorder(node.left)
-        postorder(node.right)
-        print(node.key, end = " ")
         
+    #iterative method of printing inorder
+
+    def inorder_iterative(self):
+        temp_node = self.root
+        stack = []
+        while (temp_node != None or len(stack) != 0):
+            if (temp_node != None):
+                stack.append(temp_node)
+                temp_node = temp_node.left
+            else:
+                temp_node = stack.pop()
+                print(temp_node.key, end = " ")
+                temp_node = temp_node.right
+
+        
+
+
+    # #recursive method of printing inorder
+
+    # def inorder(self, node):
+    #     if node is not None:
+            
+    #         self.inorder(node.left)
+    #         print(node.key , end = " ")
+    #         self.inorder(node.right)
+    # def print_inorder(self):
+    #     self.inorder(self.root)
+
+
+    def preorder(self, node):
+        if node is not None: 
+            print(node.key, end = " ")
+            self.preorder(node.left)
+            self.preorder(node.right)
+
+    def postorder(self, node):
+        node = self.root
+        if node is not None: 
+            self.postorder(node.left)
+            self.postorder(node.right)
+            print(node.key, end = " ")
+            
 
 
 
     
     
 def main():
-    root = None
-    root =  insert(root, 50)
+    tree = BST()
+    tree.insert(50)
 
     
-    insert(root, 30) 
+    tree.insert(30) 
   
-    insert(root, 20) 
+    tree.insert(20) 
   
-    insert(root, 40) 
+    tree.insert(40) 
   
-    insert(root, 70) 
+    tree.insert(70) 
   
-    insert(root, 60) 
+    tree.insert(60) 
   
-    insert(root, 80) 
+    tree.insert(80) 
 
-    inorder(root)
-    print()
+    tree.inorder_iterative()
 
-    preorder(root)
-    print()
-    postorder(root)
+
+   
 
 if __name__ == "__main__":
     main()
