ae30b2c1c7dec357c8191f81b756c2093309c5c8
Bamlaku <bamlakhiruya@gmail.com>
2023-10-19 23:08:28 +0300

    quick sort using middle and last as pivot

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -1,25 +1,77 @@
+
+# using first as a pivot
 def partition(l, h, arr):
     pivot = arr[l]
-    i = l
+    i = l + 1
     j = h
 
+    while True:
+        while i <= j and arr[i] < pivot:
+            i += 1
+        while i <= j and arr[j] > pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+        else:
+            break
+
+    arr[l], arr[j] = arr[j], arr[l]
+    return j
+
+
+#using second as a pivot
+def partiton2(l, h, arr):
+    pivot = arr[h]
+    i = l 
+    j = h - 1
+    while True:
+        while arr[i] < pivot:
+            i += 1
+        while arr[j] > pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+        else:
+            break
+    arr[h], arr[i] = arr[i], arr[h]
+    return i
+
+#using middle as a pivot
+def partition3(low, high, arr):
+    mid = low + (high-low) // 2
+    pivot = arr[mid]
+    i = low
+    j = high
     while True:
         while arr[i] < pivot:
             i += 1
         while arr[j] > pivot:
             j -= 1
 
-        if i >= j:
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j -= 1
+            
+        else:
+            
             return j
 
-        arr[i], arr[j] = arr[j], arr[i]
+
 
 def quick_sort(l, h, arr):
     if l < h:
-        j = partition(l, h, arr)
-        quick_sort(l, j, arr)
-        quick_sort(j + 1, h, arr)
+        i = partition3(l, h, arr)
+        quick_sort(l, i-1, arr)
+        quick_sort(i + 1, h, arr)
 
-arr = [10, 16, 8, 12, 15, 6, 3, 9, 5]
-quick_sort(0, len(arr) - 1, arr)
-print(arr)
+arr2 = [10, 6, 9, 8, 7, 2, 2, 1, 9]
+quick_sort(0, len(arr2) -1, arr2)
+print(arr2)
+
+
+
+
+
+
+
