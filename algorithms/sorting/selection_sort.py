e3dad71a1a8cb98d1990f331857216fbb6e684ef
Bamlaku <bamlakhiruya@gmail.com>
2023-10-20 00:20:07 +0300

    insertion_sort

--- algorithms/sorting/selection_sort.py
+++ algorithms/sorting/selection_sort.py
@@ -9,8 +9,21 @@ class Solution:
                     arr[i], arr[j] = arr[j], arr[i]
         return arr
     
+    def effic_selection_sort(self, arr):
+        for i in range(len(arr)):
+            min_index = i
+            for j in range(i + 1, len(arr)):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+        return arr
+
+
+    
 
     #stable selection sort (when two elements are equal the algorithm do not swap their position)
 
 solution = Solution()
 print (solution.selection_sort([64, 25, 12, 22, 11]))
+print(solution.effic_selection_sort([64, 25, 12, 22, 11]))
