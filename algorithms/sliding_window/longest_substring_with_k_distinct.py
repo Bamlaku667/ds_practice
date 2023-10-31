d99838d1340d3914dc96e5b1181a9b142134d26f
Bamlaku <bamlakhiruya@gmail.com>
2023-10-31 11:33:29 +0300

    longest_substring_using_set

--- algorithms/sliding_window/longest_substring_with_k_distinct.py
+++ algorithms/sliding_window/longest_substring_with_k_distinct.py
@@ -1,34 +1,66 @@
-def longest_substring_with_k_distinct_characters(s, k):
-  if k <= 0: 
-    return ""
+# def longest_substring_with_k_distinct_characters(s, k):
+#   if k <= 0: 
+#     return ""
   
-  max_len = 0
-  right = 0
-  left = 0 
-  char_count= {}
-  for right in range(len(s)):
-    char = s[right]
-    if char not in char_count:
-      char_count[char] = 0
-    char_count[char]  += 1
+#   max_len = 0
+#   right = 0
+#   left = 0 
+#   char_count= {}
+#   for right in range(len(s)):
+#     char = s[right]
+#     if char not in char_count:
+#       char_count[char] = 0
+#     char_count[char]  += 1
 
-    while len(char_count) > k :
+#     while len(char_count) > k :
       
-      char = s[left]
-      char_count[char] -= 1
-      if char_count[char] == 0:
-        del char_count[char]
-      left += 1
+#       char = s[left]
+#       char_count[char] -= 1
+#       if char_count[char] == 0:
+#         del char_count[char]
+#       left += 1
     
 
 
 
-    if right - left +  1 > max_len:
-      max_len = max(max_len, right-left + 1)
-      max_start = left
+#     if right - left +  1 > max_len:
+#       max_len = max(max_len, right-left + 1)
+#       max_start = left
 
-  return s[max_start: max_start + max_len]
+#   return s[max_start: max_start + max_len]
 
+
+# # Example usage:
+# s = "eceeba"
+# k = 2
+# result = longest_substring_with_k_distinct_characters(s, k)
+# print(result)  # Output: "ece"
+
+
+def longest_substring_with_k_distinct_characters(s, k):
+    if k <= 0:
+        return ""
+
+    max_len = 0
+    right = 0
+    left = 0
+    distinct_chars = set()
+    for right in range(len(s)):
+        char = s[right]
+        distinct_chars.add(char)
+
+        while len(distinct_chars) > k:
+            char = s[left]
+            counting_char = s[left:right+1].count(char)
+            if counting_char == 1:
+                distinct_chars.remove(char)
+            left += 1
+
+        if right - left + 1 > max_len:
+            max_len = right - left + 1
+            max_start = left
+
+    return s[max_start: max_start + max_len]
 
 # Example usage:
 s = "eceeba"
